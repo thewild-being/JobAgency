04ee9c8ac024e90860ac918e4a4283700ef5f4cd
Tommaso Giambelli <82868359+thewild-being@users.noreply.github.com>
2022-06-02 11:26:20 +0200

    Update urls.py

--- resume/urls.py
+++ resume/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.resume_view),
-    path('/new', views.resume_create)  # new resume can be created at localhost:8000/resume/new -> CRUD
+    path('new', views.resume_create)  # new resume can be created at localhost:8000/resume/new -> CRUD
 ]
